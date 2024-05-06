761fb4b3f6507022fcc6392c08553214cc78d360
oakbrad <brad@bradwenner.com>
2024-05-06 13:21:36 -0700

    5eTools - Change index.html mirror message to link to Dungeon Church

--- 5etools-homebrew/getbrew.sh
+++ 5etools-homebrew/getbrew.sh
@@ -9,3 +9,10 @@ rm "/home/ubuntu/5etools-homebrew/Dungeon Church; Pyora.json"
 chown opc:opc "/home/ubuntu/5etools/homebrew/Dungeon Church; Pyora.json"
 chmod 775 "/home/ubuntu/5etools/homebrew/Dungeon Church; Pyora.json"
 echo "Done loading newest brew."
+
+# This changes the alert message on the main page
+cd /home/ubuntu/5etools
+original="this is a mirror, and is not guaranteed to be up-to-date. Use at your own risk. If you can, please use the main site at"
+replacement="this is the <a class=alert-link href=https://www.dungeon.church>Dungeon Church</a> mirror that serves our <a class=alert-link href=https://github.com/oakbrad/dungeonchurch>Pyora setting</a>. The main site is at"
+sed -i "s|$original|$replacement|g" index.html
+echo "Done updating homepage"
