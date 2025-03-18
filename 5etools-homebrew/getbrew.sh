7ee8bbb79ebb8b76c89fa449a02549947baba9ad
Brad Wenner <brad@bradwenner.com>
2025-03-18 14:18:21 -0700

    Update fetch brew script

--- 5etools-homebrew/getbrew.sh
+++ 5etools-homebrew/getbrew.sh
@@ -12,7 +12,9 @@ echo "Done loading newest brew."
 
 # This changes the alert message on the main page
 cd /home/ubuntu/5etools
-original="this is a mirror, and is not guaranteed to be up-to-date. Use at your own risk. If you can, please use the main site at"
-replacement="this is the <a class=alert-link href=https://www.dungeon.church>Dungeon Church</a> mirror that serves our <a class=alert-link href=https://github.com/oakbrad/dungeonchurch>Pyora setting</a>. The main site is at"
+original='this is a mirror, and is not guaranteed to be up-to-date. Use at your own risk. If you can, please use the main site at <a class="alert-link" href="https://5e.tools/" target="_blank">5e.tools</a>.'
+replacement="this is the <a class=alert-link href=https://www.dungeon.church>Dungeon Church</a> mirror that serves our homebrew <a class=alert-link href=https://github.com/oakbrad/dungeonchurch>Pyora setting</a>, using the 2014 rules."
 sed -i "s|$original|$replacement|g" index.html
+nuke="<div class=\"mb-2\" id=\"wrp-patreon\"></div>"
+sed -i "s|$nuke| |g" index.html
 echo "Done updating homepage"
